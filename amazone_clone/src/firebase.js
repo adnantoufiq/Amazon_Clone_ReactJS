b79ab24ec636aac52ddb8a331a3cf96caefeed27
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-09-13 10:42:33 +0600

    firebase config file for authentication

---
+++ amazone_clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAYcB9T_8ZU_mQkJ6-wSYMu3-k7fRTtSXY",
+  authDomain: "reactjs-bda18.firebaseapp.com",
+  projectId: "reactjs-bda18",
+  storageBucket: "reactjs-bda18.appspot.com",
+  messagingSenderId: "662295970436",
+  appId: "1:662295970436:web:fd1b60059c2bf6bb7338d1",
+  measurementId: "G-F4038W1B5T",
+};
