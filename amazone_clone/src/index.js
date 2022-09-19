ae68023eacbc95ea6fa3498f00fa3776d62c20e9
adnantoufiq69@gmail.com <adnantoufiq69@gmail.com>
2022-09-19 09:58:11 +0600

    Modified with React-State-Provider

--- amazone_clone/src/index.js
+++ amazone_clone/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reducer, { initialState } from "./component/reducer";
+import { StateProvider } from "./component/StateProvider";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
 
